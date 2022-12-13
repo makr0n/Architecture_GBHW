0ff584b96811abaf2984e26a8fb5e2febf239897
SergeiPospelov <pospelka413@gmail.com>
2022-12-13 23:14:48 +0300

    Flash  Class created

--- src/ModelElements/Flash.java
+++ src/ModelElements/Flash.java
@@ -1,6 +1,26 @@
 package ModelElements;
 
+import java.awt.*;
+
 public class Flash {
     public Point3D Location;
+    public Angle3D Angle;
+    public Color Color;
+    public Float Power;
+
+    public Flash(Point3D Location, Angle3D Angle, Color Color, Float Power) {
+        this.Location = Location;
+        this.Angle = Angle;
+        this.Color = Color;
+        this.Power = Power;
+    }
+
+    public void Rotate(Angle3D Angle) {
+
+    }
+
+    public void Move(Point3D Point) {
+
+    }
 
 }
